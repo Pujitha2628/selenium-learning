01cfa0fe428fb6ab87418f997af865fab0910c9d
Pujitha <pujith4448@gmail.com>
2026-07-13 22:23:59 +0530

    basicconditions

---
+++ src/BasicConditions.java
@@ -0,0 +1,15 @@
+public class BasicConditions {
+
+    public static void main(String[] args) {
+
+        int i=9, j=8;
+
+        if(i<j){
+            System.out.println("you are wrong");
+        }
+        else{
+            System.out.println("Sorry");
+        }
+    }
+
+}
